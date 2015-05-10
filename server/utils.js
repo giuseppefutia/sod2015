a546ce3b419f2d56f7292e4d8e6bfa3390a6cccf
alemela <alessiom92@gmail.com>
2015-05-10 18:28:36 +0200

    Fix abs in coords

--- server/utils.js
+++ server/utils.js
@@ -4,9 +4,9 @@ var earthCirc = 40075 //in Km
 exports.calculateLimitCoords = function (lat, long) {
     var POIcoords = new Object;
 
-    POIcoords.lowLat = (0.009 * distanceKm) - lat;
+    POIcoords.lowLat = Math.abs((0.009 * distanceKm) - lat);
     POIcoords.highLat = (0.009 * distanceKm) + lat;
-    POIcoords.lowLong = ((360/(Math.cos(lat) * earthCirc)) * distanceKm) - long;
+    POIcoords.lowLong = Math.abs(((360/(Math.cos(lat) * earthCirc)) * distanceKm) - long);
     POIcoords.highLong = ((360/(Math.cos(lat) * earthCirc)) * distanceKm) + long;
 
     return POIcoords;
