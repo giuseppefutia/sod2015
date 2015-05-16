33aa0f303866d89202613983afaecbd905a1efa8
alemela <alessiom92@gmail.com>
2015-05-16 22:19:38 +0200

    Fixed navbar and removed unused code

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,7 @@
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' })
-
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-})
-
 /* smooth scrolling for nav sections */
-window.addEventListener("hashchange", function() { scrollBy(0, -70) })
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top -80
+    }, 500);
+    return false;
+});
