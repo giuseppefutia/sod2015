856c66ddc5964634215930bd1392da45b313fa2b
alemela <alessiom92@gmail.com>
2015-07-20 18:38:55 +0200

    added sparql to master to retrive subject name

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,5 +1,6 @@
 var urlFeed = "http://pentos.polito.it:8890/sparql?default-graph-uri=http://explorer.nexacenter.org/feed";
-var urlHistory = "http://pentos.polito.it:8890/sparql?default-graph-uri=http://explorer.nexacenter.org/feed";
+var urlHistory = "http://pentos.polito.it:8890/sparql?default-graph-uri=http://explorer.nexacenter.org/history";
+var urlMaster = "http://pentos.polito.it:8890/sparql?default-graph-uri=http://explorer.nexacenter.org/master";
 
 var query = "SELECT ?a ?b ?c WHERE {?a ?b ?c .}"
 var resArray = new Array();
@@ -25,17 +26,20 @@ function sparqlGet(theUrl) {
             for ( var i in resArray ) {
                 //console.log(resArray[i]);
                 if (resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'] !== "commit") {
+                  var getSubjectLabel = "SELECT ?a WHERE {<"+resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#subject']+"> <http://www.w3.org/2000/01/rdf-schema#label> ?a .}"
+                  getLabel(urlMaster+"&query="+ encodeURIComponent(getSubjectLabel) +"&format=json", function(subject){
                   var newRow = tableRef.insertRow(tableRef.rows.length);
                   var newCell0 = newRow.insertCell(0);
                   var newCell1 = newRow.insertCell(1);
                   var newCell2 = newRow.insertCell(2);
-                  var newText0 = resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#subject'] + "<br>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate'] + "<br><b>" + resArray[i].oldObject + "</b>";
-                  var newText1 = resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#subject'] + "<br>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate'] + "<br><b><span style='color:red;'>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#object'] + "</span></b><br><small><i>Modified by: " + resArray[i]['dc:Author'] +  " at: " + new Date(resArray[i]['dc:time']*1000) + "</i></small>";
+                  var newText0 = subject + "<br>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate'] + "<br><b>" + resArray[i].oldObject + "</b>";
+                  var newText1 = subject + "<br>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate'] + "<br><b><span style='color:red;'>" + resArray[i]['http://www.w3.org/1999/02/22-rdf-syntax-ns#object'] + "</span></b><br><small><i>Modified by: " + resArray[i]['dc:Author'] +  " at: " + new Date(resArray[i]['dc:time']*1000) + "</i></small>";
                   var newText2 = '<button type="button" class="btn btn-default" onclick="acceptedTriple(\''+i+'\')"><span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span></button><br><button type="button" class="btn btn-default" onclick="refusedTriple(\''+i+'\')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>';
 
                   newCell0.innerHTML = newText0;
                   newCell1.innerHTML = newText1;
                   newCell2.innerHTML = newText2;
+                  });
                 }
             }
         }
@@ -94,6 +98,16 @@ function runUpdate(theUrl) {
         success: function( _data ) {
             $("#mainTable").html("");
             sparqlGet(queryUrl);
+        }
+    });
+}
+
+function getLabel(theUrl, callback) {
+    $.ajax({
+        dataType: "jsonp",  
+        url: theUrl,
+        success: function( _data ) {
+            callback(_data.results.bindings[0].a.value);
         }
     });
 }
